41d5089a3845a610f82292203aba7fb18d86a535
Hamzanazir0 <Hamzanazir0@gmail.com>
2021-06-15 16:54:46 +0500

    asd

--- createProduct.js
+++ createProduct.js
@@ -11,4 +11,34 @@ const createProduct = async (title, price, tags) => {
   return product;
 };
 
-module.exports = createProduct;
+const updateProduct = async (id, title, price, tags) => {
+  let product = await modal.findById(id);
+  product.title = title;
+  product.price = price;
+  product.tags = tags;
+  // product.slug = slug;
+  await product.save();
+  console.log("Product Added");
+  return product;
+};
+
+const deleteProduct = async (id) => {
+  let product = await modal.findByIdAndDelete(id);
+  console.log(product);
+};
+
+const getAllProduct = async () => {
+  let products = await modal.find();
+  return products;
+};
+
+const getProduct = async (id) => {
+  let product = await modal.findById(id);
+  return product;
+};
+
+module.exports.createProduct = createProduct;
+module.exports.updateProduct = updateProduct;
+module.exports.deleteProduct = deleteProduct;
+module.exports.getAllProduct = getAllProduct;
+module.exports.getProduct = getProduct;
--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-const createProduct = require("./createProduct");
+const {
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getAllProduct,
+  getProduct,
+} = require("./createProduct");
 
 const mongo = require("mongoose");
 mongo
@@ -14,8 +20,20 @@ mongo
   })
   .then(async () => {
     console.log("Connection Succesfull with mongo");
-    let product = await createProduct("Dell", 300, ["acha hay"]);
-    console.log(product);
+    // let product = await createProduct("Lenovo", 300, ["acha hay"]);
+    // console.log(product);
+    // let updatedproduct = await updateProduct(
+    //   "60c7102e6bde222504d2685c",
+    //   "Lenovo updated",
+    //   300,
+    //   ["acha hay"]
+    // );
+    // console.log(updatedproduct);
+    // let deletedproduct = await deleteProduct("60c7102e6bde222504d2685c");
+    // let getproducts = await getAllProduct();
+    // console.log(getproducts);
+    let getproduct = await getProduct("60c65ff8ee18f70138e6403e");
+    console.log(getproduct);
   })
   .catch((err) => {
     console.log("Error Connection" + err);
